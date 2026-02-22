b97fd7448aa9ee5540374c9f860594ba78099c68
limji02 <im020418@example.com>
2026-02-22 22:31:09 +0900

    0222c

--- Algorithm/IM_prep/16811.py
+++ Algorithm/IM_prep/16811.py
@@ -1,50 +1,51 @@
-# 16811 당근 포장하기
-#
-import sys
-
-sys.stdin = open('16811.txt')
 T = int(input())
 
 for tc in range(1, T + 1):
-    N = int(input())    # N개의 당근
-    arr = sorted(list(map(int, input().split())))
+    N = int(input())    # N개의 당근 #  3<=N<=1000
+    arr = sorted(list(map(int, input().split())))  # 1<=Ci(당근 크기)<=30
 
     # N 최대가 1000 이므로...
     result = 1001
 
     # 소, 중 나누는 기준점 i
     # 중, 대 나누는 기준점 j
-    # 기준점 i와 j의 모든 경우의 수를 순회하며, 조건(크기 조건, 개수 조건)으로 필터링한다.
     for i in range(1, N-1):
         for j in range(i+1, N):
-            # 경계선에 있는 당근들 크기 같으면 탈락(continue) (*인덱스로 빈 상자는 걸러짐)
+            # 자 이제 모든 경우의 수 갑니다.
+            # 경계선에 있는 당근들 크기 같으면 리스트를 만들지 않고 다음 경우의 수로 넘어가요.
             if arr[i-1] == arr[i] or arr[j-1] == arr[j]:
                 continue
-            s_arr = arr[:i]
-            m_arr = arr[i:j]
-            l_arr = arr[j:]
+            
+            # 이 가운데 부분을 줄일 수 있을 것 같은데 잘 모르겠습니다.
+
+            s_arr = arr[:i]  # (i개) i는 1부터 시작하므로, S 바구니에 최소 1개 들어감
+            m_arr = arr[i:j]  # (j-i개) j는 i+1부터 시작하므로, M 바구니에 최소 1개 들어감
+            l_arr = arr[j:]  # (N-j개) j의 최댓값 N-1이므로, L 바구니에 최소 1개 들어감
+                                # -> 빈 바구니 없어야 한다는 조건 만족!
 
             s_count = len(s_arr)
             m_count = len(m_arr)
             l_count = len(l_arr)
 
-            # 한 상자에 N//2 초과하면 탈락
+            # 한 상자에 당근이 N//2개 초과하는지 검사
             if max(s_count, m_count, l_count) <= N//2:
-                # 두 조건 통과하면 diff 계산
+                # 검사 통과한 것들 diff 계산
                 diff = max(s_count, m_count, l_count) - min(s_count, m_count, l_count)
-                # 아래 if문이 반복되면 마지막에 최솟값이 저장된다.
+                # 아래 if문으로 최솟값 업데이트
                 if diff < result:
                     result = diff
 
-    # 단 한 번도 조건에 맞는 조합이 없어서 result가 그대로 1001이면?
+    # 업데이트 안 되고 result가 그대로 1001이면?
     if result == 1001:
         result = -1
 
+    print(f'#{tc} {result}')
 
+
+
+    # 처음에 아래와 같이 시도하다가 막막해서 버렸습니다.
     # set_C_arr = list(set(C_arr))
     # j = (N//3)*2
     # s_arr = set_C_arr[:N//3]
     # m_arr = set_C_arr[N//3:j+1]
     # l_arr = set_C_arr[j:]
-
-    print(f'#{tc} {result}')
